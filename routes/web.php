92b192789ce2fc7a232c0019470271d234ce600e
Sosa <23610074@utgz.edu.mx>
2024-10-11 13:58:14 -0600

    Project laracrud Punto inicial

--- routes/web.php
+++ routes/web.php
@@ -19,34 +19,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// ruta de vista de testing
-Route::get('/testview', function(){
-    return view('testView');
-});
-
-Route::get('/texto', function(){
-
-});
-
-Route::get('/echo', function(){
-    echo view ('test2');
-});
-
-Route::get('/MasRutas', function(){
-    return "Podemos tener mas rutas tipo get,POST
-    Delete, Patch";
-});
-
-Route::get('/custom', function(){
-   $msj = "Mensaje desde el servidor";
-   $id = "500";
-   return view  ('custom', ['msj' => $msj, "identy " => $id, "A" => 29]);
-});
-
-Route::get('/contact', function(){
-    return view('contacto');
- }) -> name('contact');
- 
-
-Route::get('/testdb', [TestController::class, 'test']);
