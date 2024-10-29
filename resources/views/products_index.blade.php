f81a9b5ae4479dd608a2a926087a151c31721772
Sosa <23610074@utgz.edu.mx>
2024-10-29 14:55:51 -0600

    Primer letra crud

--- resources/views/products_index.blade.php
+++ resources/views/products_index.blade.php
@@ -1,6 +1,8 @@
 @extends('layout.main_template')
 
 @section('content')
-<h2>Products</h2>
+<h2> Index Productos</h2>
+
+<button><a href="{{route('products.create')}}"> Crear Productos</a></button>
     
 @endsection
