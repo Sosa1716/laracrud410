e85da9c04a8e04f42c66a01ea204e94afa5d01af
Sosa <23610074@utgz.edu.mx>
2024-11-29 13:37:45 -0600

    Instalacion de Bootstrap

--- resources/views/fragments/navbar.blade.php
+++ resources/views/fragments/navbar.blade.php
@@ -1,42 +1,26 @@
-
-<style>
-    header{
-        background: rgba(79, 12, 12, 0.833);
-        padding-block: 4px;
-        margin-block: -8px;
-        margin-inline: -8px;
-    }
-
-    nav p{
-        font-family: Arial, Helvetica, sans-serif;
-        font-size: 20px;
-        padding-inline: 15px;
-
-    }
-
-    nav p a{
-        color: lemonchiffon;
-        text-decoration: none;
-        padding-block: 10px;
-        padding-inline: 10px;
-        margin-inline: -4px;
-    }
-
-    nav :hover{
-        background: rgba(83, 10, 10, 0.771);
-        padding-block: 10px;
-        padding-inline: 5px;
-    }
-</style>
-   
-        <header>
-            <nav>
-                <p>
-                    <a href="{{route('index')}}">Inicio</a>
-                    <a href="{{route('products.index')}}">Productos</a>
-                    <a href="{{route('clients.index')}}">Clientes</a>
-                    <a href="{{route('sales.index')}}">Ventas</a>
-                </p>
-            </nav>
-        </header> 
-        <br>
+    <!--NavBar de bootstrap-->
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Luis Sosa</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('index')}}">Inicio</a>
+                      </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{route('products.index')}}">Productos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{route('clients.index')}}">Clientes</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{route('sales.index')}}">Ventas</a>
+                  </li>
+                  
+                </ul>
+              </div>
+            </div>
+          </nav>
